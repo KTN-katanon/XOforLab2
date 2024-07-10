fc185ee506d0c9ce789a6da5aa2a2595e68b38ee
KTN-katanon <66160196@go.buu.ac.th>
2024-07-10 21:40:45 +0700

    Add printWelcome

--- XO_Lab2/src/main/java/com/katanon/xo_lab2/XO_Lab2.java
+++ XO_Lab2/src/main/java/com/katanon/xo_lab2/XO_Lab2.java
@@ -8,9 +8,17 @@ package com.katanon.xo_lab2;
  *
  * @author nonku
  */
+import java.util.*;
 public class XO_Lab2 {
+    private final static int SIZE = 3;
+    private static char[][] board = new char[SIZE][SIZE];
+    private static char currentPlayer = 'X';
+    private static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        printWelcome();
+    }
+    private static void printWelcome(){
+        System.out.println("Welcome to XO");
     }
 }
