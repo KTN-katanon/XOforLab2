0823430ded9f04fa493fb5600ada0eabf4a8a278
KTN-katanon <66160196@go.buu.ac.th>
2024-07-10 22:25:30 +0700

    Update XO_Lab2.java
    Add printBoard

--- XO_Lab2/src/main/java/com/katanon/xo_lab2/XO_Lab2.java
+++ XO_Lab2/src/main/java/com/katanon/xo_lab2/XO_Lab2.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.katanon.xo_lab2;
 
 /**
@@ -9,7 +8,9 @@ package com.katanon.xo_lab2;
  * @author nonku
  */
 import java.util.*;
+
 public class XO_Lab2 {
+
     private final static int SIZE = 3;
     private static char[][] board = new char[SIZE][SIZE];
     private static char currentPlayer = 'X';
@@ -18,15 +19,26 @@ public class XO_Lab2 {
     public static void main(String[] args) {
         printWelcome();
     }
-    private static void printWelcome(){
+
+    private static void printWelcome() {
         System.out.println("Welcome to XO");
     }
-    
+
     private static void createBoard() {
-        for (int i = 0; i < SIZE; i++){
-            for (int j = 0; k < SIZE; j++){
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; k < SIZE; j++) {
                 board[i][j] = '-';
             }
         }
     }
+
+    private static void printBoard() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
 }
