993fb0b473efa320a69406ca9b9175f7e0ad3fd0
KTN-katanon <66160196@go.buu.ac.th>
2024-07-10 21:32:48 +0700

    Create Project
    Create Project

---
+++ XO_Lab2/src/main/java/com/katanon/xo_lab2/XO_Lab2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.katanon.xo_lab2;
+
+/**
+ *
+ * @author nonku
+ */
+public class XO_Lab2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
