672faa3d2a76ce482adb9b9a13a659f0a3977642
KTN-katanon <66160196@go.buu.ac.th>
2024-07-10 23:03:09 +0700

    Update XO_Lab2.java
    Add checkWin and checkDraw method

--- XO_Lab2/src/main/java/com/katanon/xo_lab2/XO_Lab2.java
+++ XO_Lab2/src/main/java/com/katanon/xo_lab2/XO_Lab2.java
@@ -78,4 +78,36 @@ public class XO_Lab2 {
         }
     }
 
+    //check win
+    public static boolean checkWinner() {
+        // check row
+        if ((board[0][0] == currentPlayer && board[0][1] == currentPlayer && board[0][2] == currentPlayer)
+                || (board[1][0] == currentPlayer && board[1][1] == currentPlayer && board[1][2] == currentPlayer)
+                || (board[2][0] == currentPlayer && board[2][1] == currentPlayer && board[2][2] == currentPlayer)) {
+            return true;
+        } // check col
+        else if ((board[0][0] == currentPlayer && board[1][0] == currentPlayer && board[2][0] == currentPlayer)
+                || (board[0][1] == currentPlayer && board[1][1] == currentPlayer && board[2][1] == currentPlayer)
+                || (board[0][2] == currentPlayer && board[1][2] == currentPlayer && board[2][2] == currentPlayer)) {
+            return true;
+        } // check X
+        else if (board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer
+                || board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean checkDraw() {
+        // check draw
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (board[i][j] == '-') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
 }
